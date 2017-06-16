ac3e5f4e2ae4890c5c06e73954be5cc16f749bf5
karimata1106 <karimata1106@gmail.com>
2017-06-17 04:51:35 +0900

    XamarinTwitter

---
+++ Models/Tweet.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using CoreTweet;
+
+namespace XamarinTweet.Models
+{
+    public class Tweet
+    {
+        #region 認証パラメータ
+        private const string Consumer_Key = "Your ConsumerKey";
+        private const string Consumer_Secret = "Your ConsumerSecuret";
+        private const string Access_Token = "Your AccessToken";
+        private const string Access_Token_Secret = "Your AccessTokenSecret";
+        #endregion
+
+        #region フィールド
+        private readonly Tokens _tokens;
+        #endregion
+
+        #region コンストラクタ
+        public Tweet()
+        {
+            // API にアクセスするためのトークン群
+            _tokens = Tokens.Create(
+                Consumer_Key,
+                Consumer_Secret,
+                Access_Token,
+                Access_Token_Secret
+            );
+        }
+        #endregion
+
+        #region メソッド
+        public void SayHelloTwitter()
+        {
+            var tokenSource = new CancellationTokenSource();
+            _tokens.Statuses.UpdateAsync(
+                new { status = "Hello Twitter" },
+                tokenSource.Token
+            );
+        }
+        #endregion
+    }
+}
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using XamarinTweet.Models;
+
+namespace XamarinTweet.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        #region コマンド
+        public DelegateCommand SendTwitterCommand { get; }
+        #endregion
+
+        #region  コンストラクタ
+
+        public MainPageViewModel()
+        {
+            var tweet = new Tweet();
+            SendTwitterCommand = new DelegateCommand(() => tweet.SayHelloTwitter());
+        }
+
+        #endregion
+    }
+}
